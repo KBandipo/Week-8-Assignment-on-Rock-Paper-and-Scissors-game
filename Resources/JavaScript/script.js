0d5bf7201406e14c716938ef776856a4339082da
kennybandy <nekbandy.com>
2023-09-11 20:40:45 +0100

    State the Problem

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -1,1 +1,9 @@
 "use strict";
+//‘Rock’, ‘Paper’ or ‘Scissors’ Game
+
+/***********State Problem*********/
+//-How to Computer select at random between ‘Rock’, ‘Paper’ or ‘Scissors’ Game and return the choice
+//-How to make the choices of th player case insensitive
+//-How to compare the Player and the computer's choices in the first round
+//-How to play in rounds
+//-How to display the winner
