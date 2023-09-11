8937c8885822244c4a9a2ef14be6b44e3cc053fa
kennybandy <nekbandy.com>
2023-09-11 20:42:11 +0100

    Highligh the Pseudocode

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -7,3 +7,16 @@
 //-How to compare the Player and the computer's choices in the first round
 //-How to play in rounds
 //-How to display the winner
+
+/***********************************************************/
+
+/**********Pseudocode*********/
+//-Create an array of 'gameChopices'
+//Create Function 'playerSelection' to Prompt the player to select from the 'gameChoices' and store it and return it
+//-To make the choices of th player case insensitive
+// Create 'computerSelection' Function to make Computer picks at random between ‘Rock’, ‘Paper’ or ‘Scissors’ in the 'gameChoices'
+//Return and display the output of the Computer choice Selection using and User's selection
+//invoke 'computerSelection' and 'playerSelection'
+//Play the game once and comparing the 'computerSelection' and 'playerSelection' display the winner
+//Create Function 'game' to play 5 rounds using loop
+//Display the winner
