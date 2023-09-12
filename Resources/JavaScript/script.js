a243cd893df7129da3242dca4122fa4aba55a71e
kennybandy <nekbandy.com>
2023-09-12 18:14:31 +0100

    Import playerChoice function

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -38,28 +38,7 @@ console.log(
 /***********************************************************/
 
 /*************************Implimentatio**********************************/
-
-//Create Function 'playerSelection' to Prompt the player to select from the 'gameChoices' and store it and return it
-function playerChoice() {
-  //make the choices of th player case insensitive
-  let userInput = prompt(
-    "Select from 'Rock', 'Paper' or 'Scissors'!"
-  ).toLowerCase();
-
-  //-Create a conditions to return the seletion of player
-  if (userInput === "rock") {
-    console.log("You selected Rock");
-  } else if (userInput === "paper") {
-    console.log("You selected Paper");
-  } else if (userInput === "scissors") {
-    console.log("You selected Scissors");
-  } else {
-    console.log("Invalid Selection!");
-    console.log("Select from 'Rock', 'Paper' or 'Scissors'!");
-  }
-  return userInput;
-}
-
+import playerChoice from "./playerChoice.js";
 import getComputerChoice from "./getComputerChoice.js";
 
 //Play singleRound
