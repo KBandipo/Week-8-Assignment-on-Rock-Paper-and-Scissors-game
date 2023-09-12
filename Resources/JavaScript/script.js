a2a778c24dccbca081bb43b91082e71369022a25
kennybandy <nekbandy.com>
2023-09-12 18:18:57 +0100

    Import singleRound function

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -40,35 +40,7 @@ console.log(
 /*************************Implimentatio**********************************/
 import playerChoice from "./playerChoice.js";
 import getComputerChoice from "./getComputerChoice.js";
-
-//Play singleRound
-function singleRound() {
-  let playerSelection = playerChoice();
-  let computerSelection = getComputerChoice();
-
-  if (playerSelection === computerSelection) {
-    console.log(`It's a tie!`);
-    return "Tie";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-    return "Win";
-  } else if (
-    (computerSelection === "rock" && playerSelection === "scissors") ||
-    (computerSelection === "paper" && playerSelection === "rock") ||
-    (computerSelection === "scissors" && playerSelection === "paper")
-  ) {
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    return "Lose";
-  } else {
-    console.log(
-      `${playerSelection} is an invalid Input. Select from 'Rock', 'Paper' or 'Scissors'!`
-    );
-  }
-}
+import singleRound from "./singleRound.js";
 
 /****************************************************************/
 function round() {
